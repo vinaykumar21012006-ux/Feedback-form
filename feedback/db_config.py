edf077371b7cc1fcc940349bdba9284bce697cdc
vinaykumar21012006-ux <vinaykumar21012006@gmail.com>
2026-03-19 11:47:54 +0530

    Add files via upload

---
+++ feedback/db_config.py
@@ -0,0 +1,10 @@
+import mysql.connector
+
+
+def get_db_connection():
+    return mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password="Vinay@1234",
+        database="feedback_db"
+    )
